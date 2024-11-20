a393a886487635b5a213a4a0cc120ea3b1d62d9c
thesystemsmith <abdulrasheed.techy@gmail.com>
2024-11-21 02:17:00 +0530

    setup node server for the html markdown editor

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const marked = require('marked');
+
+const app = express()
+
+app.use(cors)
+app.use(bodyParser.json())
+
+// POST endpoint for Markdown to HTML conversion
+app.post('/convert', (req, res) => {
+    const {markdown} = req.body
+
+    if(!markdown){
+        return res.status(400).send({
+            error: 'markdown input is required'
+        })
+    }
+
+    const html = marked(markdown)
+    res.send({html})
+})
+
+const PORT = 5001
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
